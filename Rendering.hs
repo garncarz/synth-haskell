be26c7add5ad225d4ee163baf3997190761e5041
Ondřej Garncarz <ondrej@garncarz.cz>
2012-10-28 02:14:19 +0000

    Play pouze jako možnost z příkazové řádky (občas nedohraje do konce a jako by bylo přehráváno s nižší sample rate), fade out na konci tónů (proti clippingu, ale stále není plně čistý zvuk).

--- Rendering.hs
+++ Rendering.hs
@@ -19,8 +19,17 @@ triangle :: SampleFunc
 triangle freq _ time = (\x -> 2 * x - 1) . abs .
 	(\x ->  x - (fromIntegral . floor) x) $ freq * time
 
+-- TODO zkusit druhý kvadrant sinusoidy pro sestup
+fadeOutVol :: Time -> Time -> Sample
+fadeOutVol dur time = if fromFading < 0 then 1 else fading where
+	fading = 1 - (fromFading / fadingTime)
+	fromFading = time - ((1 - fadingPart) * dur)
+	fadingTime = fadingPart * dur
+	fadingPart = 0.2
+
 nice :: SampleFunc -> SampleFunc
-nice func freq dur time = exp (-2 * time / dur) * func freq dur time
+nice func freq dur time = exp (-2 * time / dur) * func freq dur time *
+	fadeOutVol dur time
 
 
 render :: SampleFunc -> Tone -> FrameStream
--- Synth.hs
+++ Synth.hs
@@ -7,14 +7,16 @@ import Rendering
 import Types
 import Wave
 
+import Control.Monad
 import System.Console.CmdArgs
 
-data Args = Args {input :: String, output :: String}
+data Args = Args {input :: String, output :: String, play :: Bool }
 	deriving (Show, Data, Typeable)
 
 argsDefs = Args {
 	input = def &= typ "INPUT" &= argPos 0 &= opt "input.midi",
-	output = def &= typ "OUTPUT" &= argPos 1 &= opt "output.wav" }
+	output = def &= typ "OUTPUT" &= argPos 1 &= opt "output.wav",
+	play = False &= typ "BOOL" }
 	&= program "synth"
 
 
@@ -23,12 +25,16 @@ main = do
 	Args{..} <- cmdArgs argsDefs
 
 	(tones, dur) <- loadMidi input
+	putStrLn $ "MIDI file " ++ input ++ " loaded, " ++ show (length tones) ++
+		" tones."
+	
 	let
 		samples = map (\(time, tone) -> timeShift time $
-			render (nice square) tone) tones
+			render (nice sine) tone) tones
 		empty = emptyTrack dur
 		finalAudio = foldl1 sumStreams (empty:samples)
 	saveWave output finalAudio
+	putStrLn $ "WAVE file " ++ output ++ " saved."
 	
-	play finalAudio samplingRate
+	when play $ Play.play finalAudio samplingRate
 
