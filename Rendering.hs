7735495d8fe56542d7916fd722244c1aeaa9f4d0
Ondřej Garncarz <ondrej@garncarz.cz>
2012-10-30 00:20:32 +0000

    Zavedení harmonických frekvencí (podle čísla kanálu).

--- Rendering.hs
+++ Rendering.hs
@@ -21,6 +21,18 @@ triangle :: SampleFunc
 triangle freq _ time = (\x -> 2 * x - 1) . abs .
 	(\x ->  x - (fromIntegral . floor) x) $ freq * time
 
+harmonicsVolumes :: [[Volume]]
+harmonicsVolumes = [
+	[1, -0.5, 0.2],
+	[-1, -0.2, -0.5],
+	[0.5, 1],
+	[-1, 0.8, 0.6, -0.1]
+	]
+
+harmonics :: [Volume] -> SampleFunc -> SampleFunc
+harmonics vols func freq dur time = sum $ map (\(vol, mul) ->
+	vol * func (mul * freq) dur time) $ zip vols [1..]
+
 
 sawVol :: VolumeFunc
 sawVol _ time = (\x -> x - (fromIntegral . floor) x) $ time * 10
@@ -38,22 +50,29 @@ expFadeOut dur time = exp $ log 0.05 * time / dur
 cosFadeOut :: VolumeFunc
 cosFadeOut dur time = cos $ acos 0 * time / dur
 
-
-nice :: SampleFunc -> SampleFunc
-nice func freq dur time = func freq dur time * cosFadeOut dur time
+nice :: VolumeFunc
+nice = cosFadeOut
 
 
 instrumentFunc :: Tone -> SampleFunc
 instrumentFunc tone = renderingFuncs !! (instrument tone `mod`
 	length renderingFuncs)
 
+instrumentHarmonics :: Tone -> [Volume]
+instrumentHarmonics tone = harmonicsVolumes !! (channel tone `mod`
+	length harmonicsVolumes)
+
 
 render :: Tone -> FrameStream
 render tone = array (0, frames)
-	[(frame, vol * nice func freq dur (time frame)) | frame <- range(0, frames)]
+	[(frame, vol * nice dur (time frame) *
+		harmonics vols func freq dur (time frame))
+		| frame <- range(0, frames)]
 	where
 		frames = ceiling $ realSamplingRate * dur
 		freq = pitch tone; dur = duration tone; vol = volume tone
 		time frame = fromIntegral frame / realSamplingRate
-		func = instrumentFunc tone
+		-- func = instrumentFunc tone
+		func = sine
+		vols = instrumentHarmonics tone
 
