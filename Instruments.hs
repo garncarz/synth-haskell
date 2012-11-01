40e688bc7a8f2c21678586ab2087047417791ecc
Ondřej Garncarz <ondrej@garncarz.cz>
2012-11-01 02:43:41 +0000

    Kompletace instrumentů pro všechny MIDI presety.

--- Instruments.hs
+++ Instruments.hs
@@ -16,25 +16,29 @@ instruments = [
 	Instrument { name = "piano",
 		isPreset = between 0 8,
 		sampler = simple [1, -0.5, 0.2] },
-	Instrument { name = "idiophone",
-		isPreset = between 9 15,
+	Instrument { name = "steel",
+		isPreset = between 9 16,
 		sampler = simple [0.5, 1] },
-	
 	Instrument { name = "organ",
 		isPreset = between 17 21,
 		sampler = simple [-0.5, 0.3, -0.25] },
-	
+	Instrument { name = "concertina",
+		isPreset = between 22 24,
+		sampler = simple [-0.8, 0.2, -0.4, 0.02] },
+	Instrument { name = "ac. guitar",
+		isPreset = between 25 26,
+		sampler = simple [1, 0.2, -0.4] },
 	Instrument { name = "el. guitar",
 		isPreset = between 27 32,
 		sampler = simple [-1, 0.8, 0.6, -0.1] },
 	Instrument { name = "bass",
 		isPreset = between 33 40,
 		sampler = simple [-1, -0.2, -0.5] },
-	
 	Instrument { name = "strings",
 		isPreset = \p -> between 41 52 p && notElem p [47, 48],
 		sampler = simple [0.5, -0.3, 1] },
-	
+	defaultInstrument { name = "harp", isPreset = (==) 47 },
+	defaultInstrument { name = "timpani", isPreset = (==) 48 },
 	Instrument { name = "voice",
 		isPreset = between 53 55,
 		sampler = simple [1, -0.6, 0.4, -0.8] },
@@ -58,7 +62,11 @@ instruments = [
 		sampler = simple [0.33, -0.3, 0.44, -0.44] },
 	Instrument { name = "exotic",
 		isPreset = between 105 112,
-		sampler = simple [-0.9, 0.3, 0.25, -0.4] }
+		sampler = simple [-0.9, 0.3, 0.25, -0.4] },
+	Instrument { name = "exotic drums",
+		isPreset = between 113 119,
+		sampler = simple [-1, -0.15, 0.6] },
+	defaultInstrument { name = "crazy", isPreset = (>) 119 }
 	]
 
 defaultInstrument = head instruments :: Instrument
